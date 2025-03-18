7dc3567d98a6b164cbd5669863cf131730a4a9d3
Moses Natukwatsa <moses.na.43@gmail.com>
2025-03-18 10:48:11 +0300

    Commit #2
    In the New Version from line 1, 9 lines were involved in the change

---
+++ Abstract Work/src/employee.java
@@ -0,0 +1,9 @@
+public class employee extends team {
+    public void printInfo(){
+         String name = "Shaggs";
+         String title = "Manager";
+         int age = 24;
+
+         System.out.println("Name:- " + name + "Manager:- " + title + "Age:- " + age);
+    }
+}
