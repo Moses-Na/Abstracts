992695b8a8bb6a7294a4f18dd3e83e11d1d5f5e5
Moses Natukwatsa <moses.na.43@gmail.com>
2025-03-18 11:49:19 +0300

    Commit #2
    In the Old Version on line 1, 5 lines were involved in the change. And in the New Version on line 1, 13 lines were involved in the change.

--- Abstract Work/src/Main.java
+++ Abstract Work/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[]args){
-        
+    public static void main(String[] args) {
+
+        team[] team = {
+                new Company("RestAssurance ", "Insurance Co. ", 300000.0),
+                new employee("Shaggs ", "Insurance Manager", "Grad Stu") {
+                    @Override
+                    public void printInfo() {
+                    }
+                }
+        };
     }
 }
